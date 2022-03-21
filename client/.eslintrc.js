71797ad36e303bb337088abf5b4318041d025804
elephantMax <hatypovm12@mail.ru>
2022-03-21 15:13:38 +0300

    init client

---
+++ client/.eslintrc.js
@@ -0,0 +1,26 @@
+/* eslint-env node */
+require("@rushstack/eslint-patch/modern-module-resolution");
+
+module.exports = {
+  "root": true,
+  "extends": [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+  ],
+  "env": {
+    "vue/setup-compiler-macros": true
+  },
+  "rules": {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-var': 'error',
+    semi: ['error', 'never'],
+    quotes: ['error', 'single', { allowTemplateLiterals: true }],
+    'no-multi-spaces': 'error',
+    'space-in-parens': 'error',
+    'no-multiple-empty-lines': 'error',
+    'prefer-const': 'error',
+    'no-use-before-define': 'error',
+    'vue/multi-word-component-names': ['error'],
+  }
+}
---
+++ client/src/App.vue
@@ -0,0 +1,11 @@
+<script></script>
+
+<template>
+  <div>
+    <header>
+      <span>App</span>
+    </header>
+
+    <RouterView />
+  </div>
+</template>
