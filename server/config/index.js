9e8cb81ec16f825fd117b2c47f769cedd9d52e49
Fedos16 <41968433+Fedos16@users.noreply.github.com>
2022-06-08 09:18:32 +0300

    08.06.22 09:18

--- server/config/index.js
+++ server/config/index.js
@@ -3,8 +3,8 @@ require('dotenv').config()
 const { PORT, MONGO_URL, FRONTEND, NODE_ENV } = process.env
 
 module.exports = {
-  port: PORT,
-  mongo_url: MONGO_URL,
-  frontend: FRONTEND,
-  IS_PRODUCTION: NODE_ENV === 'production'
+  PORT,
+  MONGO_URL,
+  FRONTEND,
+  IS_PRODUCTION: NODE_ENV === 'production',
 }
--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
-const path = require('path');
 
 //utils
 const Logger = require('./utils/logger')
@@ -21,24 +20,20 @@ const errorMiddleware = require('./middleware/error-middleware')
 const app = express()
 
 if (config.IS_PRODUCTION) {
-  const serveStatic = require('serve-static');
-  const history = require('connect-history-api-fallback');
+  const serveStatic = require('serve-static')
+  const history = require('connect-history-api-fallback')
 
-  app.use(history());
+  app.use(history())
   app.use(serveStatic(path.join(__dirname, 'client')))
 }
 
 if (!config.IS_PRODUCTION) {
-  const cors = require('cors');
-
   const corsOptions = {
-      origin: config.frontend
+    origin: config.FRONTEND,
   }
 
-  app.use(cors(corsOptions));
+  app.use(cors(corsOptions))
 }
-
-
 app.use(express.json())
 
 //routes
@@ -46,17 +41,18 @@ app.use('/api/directory', directory)
 app.use('/api/edition', edition)
 app.use('/api/pricelist', pricelist)
 app.use('/api/outlay', outlay)
+app.use('/pdf', express.static(__dirname + '/pdf'))
 
 //middleware
 app.use(errorMiddleware)
 
-app.listen(config.port, async () => {
+app.listen(config.PORT, async () => {
   try {
-    await mongoose.connect(config.mongo_url)
+    await mongoose.connect(config.MONGO_URL)
   } catch (error) {
     Logger.log('ERROR', 'Ошибка при подключении к базе')
   }
-  console.log(`successfully connected to mongoDb ${config.mongo_url}`)
+  console.log(`successfully connected to mongoDb ${config.MONGO_URL}`)
 
-  console.log(`app started on port ${config.port}`)
+  console.log(`app started on port ${config.PORT}`)
 })
